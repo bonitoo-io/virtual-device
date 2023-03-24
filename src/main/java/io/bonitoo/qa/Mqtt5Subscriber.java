0946fd9391232ac6c70a2dab04a924f5d6f96310
karel rehor <karl.koerner@bonitoo.io>
2023-03-24 17:22:56 +0100

    chore: starting linting

--- src/main/java/io/bonitoo/qa/Mqtt5Subscriber.java
+++ src/main/java/io/bonitoo/qa/Mqtt5Subscriber.java
@@ -7,14 +7,29 @@ import com.hivemq.client.mqtt.mqtt5.message.connect.connack.Mqtt5ConnAck;
 import com.hivemq.client.mqtt.mqtt5.message.subscribe.suback.Mqtt5SubAck;
 
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
 public class Mqtt5Subscriber {
 
+    static final String defaultTopic = "test/#";
+
     // todo pass in topicFilter string as arg or in config
     static public void main(String[] args){
+
+        String topic = System.getProperty("sub.topic") == null ?
+                defaultTopic :
+                System.getProperty("sub.topic");
+
+        for(String arg : args){
+            System.out.println(arg);
+            if(arg.matches("^topic=.*")){
+                topic = arg.split("=")[1];
+            }
+        }
+
         Mqtt5BlockingClient client = Mqtt5Client.builder()
                 .identifier(UUID.randomUUID().toString())
                 .identifier("testClient")
@@ -25,8 +40,10 @@ public class Mqtt5Subscriber {
 
         System.out.println("DEBUG connect ack " + ack);
 
+        System.out.println("DEBUG subscribing to topic " + topic);
+
         CompletableFuture<Mqtt5SubAck> futureSubAck = client.toAsync().subscribeWith()
-                .topicFilter("test/#")
+                .topicFilter(topic)
                 .qos(MqttQos.AT_LEAST_ONCE)
                 .callback(publish -> {
                     String content = StandardCharsets.UTF_8.decode(publish.getPayload().get()).toString();
