792ec45177f486e7c6b30d135fe1478594de6a8e
karel rehor <karl.koerner@bonitoo.io>
2023-03-30 14:46:56 +0200

    chore: fix error redirect in script

--- scripts/broker.sh
+++ scripts/broker.sh
@@ -69,7 +69,7 @@ function start_mosquitto(){
     return
   fi
 
-  docker rm mosquito-mq > /dev/null
+  docker rm mosquito-mq > /dev/null 2>&1
   docker run --name mosquito-mq -p 1883:1883 -p 9001:9001 -v ./mosquitto/config/mosquitto.conf:/mosquitto/config/mosquitto.conf -v ./mosquitto/data:/mosquitto/data eclipse-mosquitto
 
 }
